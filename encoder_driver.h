a05437d0caaa31e60b00c9b7729bdf1ee0c8b6d6
Linsheng Pan <141114823+ray-uncoding@users.noreply.github.com>
2023-09-27 01:46:06 +0800

    Fixed encoder pin announcement issue

--- encoder_driver.h
+++ encoder_driver.h
@@ -6,12 +6,12 @@
 #ifdef ARDUINO_ENC_COUNTER
   //below can be changed, but should be PORTD pins; 
   //otherwise additional changes in the code are required
-  #define LEFT_ENC_PIN_A 2  //pin 2, CLK
-  #define LEFT_ENC_PIN_B 3  //pin 3, D
+  #define LEFT_ENC_PIN_A PD2  //pin 2, CLK
+  #define LEFT_ENC_PIN_B PD3  //pin 3, D
   
   //below can be changed, but should be PORTC pins
-  #define RIGHT_ENC_PIN_A A4  //pin A4, CLK
-  #define RIGHT_ENC_PIN_B A5   //pin A5, D
+  #define RIGHT_ENC_PIN_A PD4  //pin A4, CLK
+  #define RIGHT_ENC_PIN_B PD5   //pin A5, D
 #endif
    
 long readEncoder(int i);
